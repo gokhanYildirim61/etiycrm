eeb339c1a8dfbc74310f4b32df72f2ec2a4fb1aa
EnesCANNOT <enesscann98@gmail.com>
2024-05-10 15:06:21 +0300

    add ContactMedium Rules

---
+++ customerservice/src/main/java/com/etiyacrm/customerservice/services/rules/ContactMediumRules.java
@@ -0,0 +1,16 @@
+package com.etiyacrm.customerservice.services.rules;
+
+import com.etiyacrm.customerservice.core.exceptions.types.BusinessException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class ContactMediumRules {
+    public void checkDeletedDate(LocalDateTime localDateTime) {
+        if (localDateTime != null){
+            throw new BusinessException("The Contact Medium not found");
+        }
+    }
+}
