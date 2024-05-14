c93d016d077460125262989f7f688f33ab962842
can.ertem <can.ertem@etiya.com>
2024-05-14 15:52:58 +0300

    catalogservice typo fix

--- catalogservice/src/main/java/com/etiyacrm/catalogservice/services/mappers/CatalogMapper.java
+++ catalogservice/src/main/java/com/etiyacrm/catalogservice/services/mappers/CatalogMapper.java
@@ -11,22 +11,16 @@ import org.mapstruct.factory.Mappers;
 public interface CatalogMapper {
     CatalogMapper INSTANCE = Mappers.getMapper(CatalogMapper.class);
 
-    //@Mapping(source = "customer.id", target = "customerId")
     GetAllCatalogResponse getAllCatalogResponseFromCatalog(Catalog catalog);
 
-    //@Mapping(source = "customerId", target = "customer.id")
-    //@Mapping(source = "districtId", target = "district.id")
+    //asd
     Catalog catalogFromCreateCatalogRequest(CreateCatalogRequest createCatalogRequest);
-    //@Mapping(source = "customer.id", target = "customerId")
-    //@Mapping(source = "district.id", target = "districtId")
+
     CreatedCatalogResponse createdCatalogResponseFromCatalog(Catalog catalog);
     Catalog catalogFromUpdateCatalogRequest(UpdateCatalogRequest updateCatalogRequest);
-    //@Mapping(source = "customer.id", target = "customerId")
-    //@Mapping(source = "district.id", target = "districtId")
+
     UpdatedCatalogResponse updatedCatalogResponseFromCatalog(Catalog catalog);
     DeletedCatalogResponse deletedCatalogResponseFromCatalog(Catalog catalog);
 
-    //@Mapping(source = "customer.id", target = "customerId")
-    //@Mapping(source = "district.id", target = "districtId")
     GetCatalogResponse getCatalogResponseFromCatalog(Catalog catalog);
 }
