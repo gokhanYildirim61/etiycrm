54fb1fef12c1916a14cef7630e8ce1327a1e9a45
Mert Erdoğan <nagodretrem@gmail.com>
2024-05-14 11:56:05 +0300

    added ComponentScan

--- catalogservice/src/main/java/com/etiyacrm/catalogservice/CatalogserviceApplication.java
+++ catalogservice/src/main/java/com/etiyacrm/catalogservice/CatalogserviceApplication.java
@@ -2,8 +2,10 @@ package com.etiyacrm.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.etiyacrm.catalogservice", "com.etiyacrm.common"})
 public class CatalogserviceApplication {
 
     public static void main(String[] args) {
--- customerservice/src/main/java/com/etiyacrm/customerservice/CustomerserviceApplication.java
+++ customerservice/src/main/java/com/etiyacrm/customerservice/CustomerserviceApplication.java
@@ -2,8 +2,10 @@ package com.etiyacrm.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.etiyacrm.customerservice", "com.etiyacrm.common"})
 public class CustomerserviceApplication {
 
 	public static void main(String[] args) {
