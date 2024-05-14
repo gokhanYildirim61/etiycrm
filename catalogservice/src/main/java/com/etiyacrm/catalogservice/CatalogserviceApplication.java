827065bf3a3986132fade9aa050c53b43e763042
Mert Erdoğan <nagodretrem@gmail.com>
2024-05-14 11:12:47 +0300

    added catalogservice

---
+++ catalogservice/src/main/java/com/etiyacrm/catalogservice/CatalogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.etiyacrm.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogserviceApplication.class, args);
+    }
+
+}
