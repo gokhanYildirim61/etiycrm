9aa6dc448544657c3f49be3d9138931d9fb7de11
Mert Erdoğan <nagodretrem@gmail.com>
2024-05-23 20:51:21 +0300

    minor update

--- customerservice/src/main/java/com/etiyacrm/customerservice/services/dtos/requests/contactMedium/UpdateContactMediumRequest.java
+++ customerservice/src/main/java/com/etiyacrm/customerservice/services/dtos/requests/contactMedium/UpdateContactMediumRequest.java
@@ -16,13 +16,11 @@ public class UpdateContactMediumRequest {
     @NotBlank(message = "Customer ID is required!")
     private String customerId;
     @NotBlank(message = "Mobile Phone is required!")
-    @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)?\\d{10}$", message = "Invalid mobile phone number format!")
+    @Pattern(regexp = "^(05|5)([0-9]{9})$", message = "Invalid mobile phone format!")
     private String mobilePhone;
     @NotBlank(message = "Email is required!")
     @Email(message = "Invalid email format!")
     private String email;
-    @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)?\\d{10}$", message = "Invalid home phone number format!")
     private String homePhone;
-    @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)?\\d{10}$", message = "Invalid fax number format!")
     private String fax;
 }
